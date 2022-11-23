638bc7649140a785732cd4b19c084f2688b72e12
Luke <Infinitybeond1@protonmail.com>
2022-11-22 20:41:09 -0500

    feat: check if params are provided

--- src/urban.nim
+++ src/urban.nim
@@ -1,15 +1,19 @@
 import puppy, os, jsony, terminal
 
-if getEnv("URBAN_API_KEY", "") == "":
-  stderr.writeLine("No API key found, please set the URBAN_API_KEY environment variable")
-  quit 1
-
-type 
-  Def = object 
+type
+  Def = object
     definition: string
     author: string
   Res = object
     list: seq[Def]
+
+if getEnv("URBAN_API_KEY", "") == "":
+  stderr.writeLine("No API key found, please set the URBAN_API_KEY environment variable")
+  quit 1
+
+if paramCount() < 1:
+  stderr.writeLine("Please provide atleast one word to define")
+  quit 1
 
 let key = getEnv("URBAN_API_KEY", "")
 let query = paramStr(1)
@@ -17,7 +21,7 @@ let req = Request(
   url: parseUrl("https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" & query),
   verb: "get",
   headers: @[
-    Header(key: "X-RapidAPI-Key", value: key), 
+    Header(key: "X-RapidAPI-Key", value: key),
     Header(key: "X-RapidAPI-Host", value: "mashape-community-urban-dictionary.p.rapidapi.com")
   ]
 )
