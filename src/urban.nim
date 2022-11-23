4e6696cf9e3828878d1bb48eab9aac4be6375974
Luke <Infinitybeond1@protonmail.com>
2022-11-22 20:36:46 -0500

    Inital Commit

---
+++ src/urban.nim
@@ -0,0 +1,28 @@
+import puppy, os, jsony, terminal
+
+if getEnv("URBAN_API_KEY", "") == "":
+  stderr.writeLine("No API key found, please set the URBAN_API_KEY environment variable")
+  quit 1
+
+type 
+  Def = object 
+    definition: string
+    author: string
+  Res = object
+    list: seq[Def]
+
+let key = getEnv("URBAN_API_KEY", "")
+let query = paramStr(1)
+let req = Request(
+  url: parseUrl("https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" & query),
+  verb: "get",
+  headers: @[
+    Header(key: "X-RapidAPI-Key", value: key), 
+    Header(key: "X-RapidAPI-Host", value: "mashape-community-urban-dictionary.p.rapidapi.com")
+  ]
+)
+let res = fetch(req)
+let json = res.body.fromJson(Res)
+for item in json.list:
+  stdout.styledWriteLine(fgBlue, "Definition from " & item.author)
+  echo item.definition & "\n"
---
+++ urban.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "Luke"
+description   = "A new awesome nimble package"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["urban"]
+
+
+# Dependencies
+
+requires "nim >= 1.4.8", "puppy", "jsony"
